411e3fd162b0b83dc8086715b80808113fdc21a8
peeyush <peeyush@fizzysoftware.com>
2014-09-28 16:59:14 +0530

    mailcatcher settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
@@ -34,8 +36,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.delivery_method = :letter_opener
-  config.mail_queue_outbound_delivery_method = :letter_opener_web
+
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.mail_queue_outbound_delivery_method = :letter_opener_web
+
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
