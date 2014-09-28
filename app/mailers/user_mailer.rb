53670982feb150a6697adfc349435c9f630ad535
peeyush <peeyush@fizzysoftware.com>
2014-09-28 23:02:37 +0530

    added mail after creation of contact user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@sublet.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -12,10 +12,10 @@ class UserMailer < ActionMailer::Base
   end
 
   def tenant_response_received(owner_email, tenant_response)
-    tenant_email = tenant_response.email
+    @tenant_email = tenant_response.email
     @owner_email = owner_email
     @tenant_response = tenant_response
-    mail to: owner_email, subject: "#{tenant_email} is Interested in your space"
+    mail to: owner_email, subject: "#{@tenant_email} is Interested in your space"
   end
 
 end
--- app/models/space_request.rb
+++ app/models/space_request.rb
@@ -1,4 +1,5 @@
 class SpaceRequest < ActiveRecord::Base
+
 end
 
 # == Schema Information
