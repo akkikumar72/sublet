f8fb7291b2004e334777295fb313af09ab854634
peeyush <peeyush@fizzysoftware.com>
2014-10-02 22:20:47 +0530

    fixed typo and change tenant responses to uuid

--- db/migrate/20140928120356_create_tenant_responses.rb
+++ db/migrate/20140928120356_create_tenant_responses.rb
@@ -1,7 +1,7 @@
 class CreateTenantResponses < ActiveRecord::Migration
   def change
-    create_table :tenant_responses do |t|
-      t.integer :space_id
+    create_table :tenant_responses, id: :uuid do |t|
+      t.uuid :space_id
       t.string :email
       t.text :description
 
