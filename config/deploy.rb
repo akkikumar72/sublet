d4bed08f23ebc6cfaf59f16c28807eab48a82e0d
peeyush <peeyush@fizzysoftware.com>
2014-10-05 00:25:21 +0530

    added carreirwave linking

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,3 +54,9 @@ namespace :deploy do
 end
 
 
+namespace :carrierwave do
+  task :symlink, roles: :app do
+    run "ln -nfs #{shared_path}/uploads/ #{release_path}/public/uploads"
+  end
+  after "deploy:finalize_update", "carrierwave:symlink"
+end
