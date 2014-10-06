c0b882e54e91349bde8d11322ea98357a3d8d1ea
Mohit Jain <jain.mohit27@gmail.com>
2014-10-06 23:04:02 +0530

    tweaks done

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,17 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6LdYdfsSAAAAAJKFIdYpf0b1nUaksumY1PwYnIyl'
-  config.private_key = '6LdYdfsSAAAAAFQpN0ExDzFricPr1kLTfnv_ZAXq'
+if Rails.env.development?
+
+  Recaptcha.configure do |config|
+    config.public_key  = '6LdYdfsSAAAAAJKFIdYpf0b1nUaksumY1PwYnIyl'
+    config.private_key = '6LdYdfsSAAAAAFQpN0ExDzFricPr1kLTfnv_ZAXq'
+  end
+
+else
+
+  Recaptcha.configure do |config|
+    config.public_key  = '6Lfai_sSAAAAAJJ0PuyHMuoeLEPK5iHqgA1y8Azp'
+    config.private_key = '6Lfai_sSAAAAAG7VSB3yjQPlF1b80lzeIcM90JRw'
+  end
+
 end
+
+
